ff8ae9eee1a98af2d9fc19f3141acd51f81d6570
Pablo Molnar <pablomolnar@gmail.com>
2012-05-31 17:03:00 -0300

    Increment version number

--- RabbitHaGrailsPlugin.groovy
+++ RabbitHaGrailsPlugin.groovy
@@ -5,7 +5,7 @@ import org.apache.commons.lang.WordUtils
 
 class RabbitHaGrailsPlugin {
     // the plugin version
-    def version = "0.1"
+    def version = "0.2"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
